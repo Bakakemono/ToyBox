50d42d788e3f676180d97237c8b019251b2644f7
Bakakemono <32707953+Bakakemono@users.noreply.github.com>
2025-03-13 16:04:06 +0100

    Started creating a basic AI for some patroller

--- Assets/Scripts/Behaviors/RobotBehavior.cs
+++ Assets/Scripts/Behaviors/RobotBehavior.cs
@@ -1,14 +1,27 @@
+using UnityEditor;
 using UnityEngine;
 
 public class RobotBehavior : BaseBehaviorTree {
-
+    Transform _transform;
+    Rigidbody _rigidbody;
     Sequence _tree;
 
     private void Awake() {
-        _tree = new Sequence(this);
+        _transform = transform;
+        _rigidbody = GetComponent<Rigidbody>();
+        SetupBehaviorTree();
     }
 
     private void Update() {
+        
+    }
+
+    void SetupBehaviorTree() {
+        _tree = new Sequence(this);
+        
+
+        Sequence searchEnemy = new Sequence(this);
+        
         
     }
 }
