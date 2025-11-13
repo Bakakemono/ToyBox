fd1064107290741452808de54037e463628ad872
Bakakemono <32707953+Bakakemono@users.noreply.github.com>
2025-11-13 16:46:47 +0100

    trying to do custom shader for smooth transition

--- Assets/Scripts/SceneLoading/SceneLoaderManager.cs
+++ Assets/Scripts/SceneLoading/SceneLoaderManager.cs
@@ -10,20 +10,21 @@ public class SceneLoaderManager : MonoBehaviour
     [SerializeField] string _sceneNameToLoad;
 
     private void Update() {
-
+        
         if(Input.GetKeyDown(KeyCode.Space)) {
             StartCoroutine(LoadAsyncScene(_sceneNameToLoad));
         }
     }
 
     IEnumerator LoadAsyncScene(string sceneName) {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
-        asyncLoad.allowSceneActivation = false;
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        //asyncLoad.allowSceneActivation = false;
+        
 
         // Wait until the asynchronous scene fully loads
         while(!asyncLoad.isDone) {
             _progressText.text = asyncLoad.progress * 100f + "%";
-            yield return null;
+            yield return new WaitForSeconds(1f);
         }
     }
 }
